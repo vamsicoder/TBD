3a782c51edc8e2107f099726f36c03d9c8ab76d7
Raghuvamsi <raghuvamsi.chandrabhatla@kore.com>
2016-02-29 20:11:02 +0530

    Fixed extra hash issue by making html5Mode to true in location provider

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,27 @@
 	var app = angular.module("test_angular", ["app.controller", "ngRoute"]);
 	
-	app.config(function($routeProvider) {
-		$routeProvider
-			.when("/", {
-				templateUrl: "forms/loginForm.html",
-				controller: "loginController"
-			})
-			.otherwise({
-				redirectTo: "/"
-			})
-	})
+	// app.config(function($routeProvider) {
+	// 	$routeProvider
+	// 		.when("/", {
+	// 			templateUrl: "forms/loginForm.html",
+	// 			controller: "loginController"
+	// 		})
+	// 		.otherwise({
+	// 			redirectTo: "/"
+	// 		})
+	// });
 
+	app.config(function($locationProvider, $routeProvider) {
+	  $locationProvider.html5Mode({
+		  enabled: true,
+		  requireBase: false
+		});
+	  $routeProvider
+		.when("/", {
+			templateUrl: "forms/loginForm.html",
+			controller: "loginController"
+		})
+		.otherwise({
+			redirectTo: "/"
+		})
+	});
