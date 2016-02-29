ff8e6b0e0fe78b6fc52d26ba7e4f04aa99629772
Raghuvamsi <raghuvamsi.chandrabhatla@kore.com>
2016-02-29 23:32:42 +0530

    Added one more route /diary for Diary page

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,5 @@
 	var app = angular.module("test_angular", ["app.controller", "ngRoute"]);
 	
-	// app.config(function($routeProvider) {
-	// 	$routeProvider
-	// 		.when("/", {
-	// 			templateUrl: "forms/loginForm.html",
-	// 			controller: "loginController"
-	// 		})
-	// 		.otherwise({
-	// 			redirectTo: "/"
-	// 		})
-	// });
-
 	app.config(function($locationProvider, $routeProvider) {
 	  $locationProvider.html5Mode({
 		  enabled: true,
@@ -20,6 +9,10 @@
 		.when("/", {
 			templateUrl: "forms/loginForm.html",
 			controller: "loginController"
+		})
+		.when("/diary", {
+			templateUrl: "forms/diary.html",
+			controller: "diaryController"
 		})
 		.otherwise({
 			redirectTo: "/"
---
+++ app/js/forms/app.controllers.js
@@ -0,0 +1,30 @@
+	var app = angular.module("app.controller", []);
+
+	app.controller("loginController", ["$scope", "$http", "$location", function($scope, $http, $location) {		
+		$scope.uname = "";
+		$scope.pwd = '';
+
+		$scope.resetLogin = function() {
+			$scope.uname = "";
+			$scope.pwd = '';
+		}
+
+		$scope.login = function() {
+			var uname = $scope.uname;
+			var pwd = $scope.pwd;					
+			 if(uname.length && pwd.length) {
+			 	$http.post('http://localhost:3000/api/users ',  {name: uname, password: pwd}).then(function(res) {		
+			 		alert("saved Succesfully");
+					$scope.resetLogin();
+					debugger;
+					$location.path("/diary");
+
+				});
+			}
+		};
+	}]);
+
+	app.controller("diaryController", function($scope) {
+		$scope.message = "Hiiii diary am back";
+	});
+ 	
